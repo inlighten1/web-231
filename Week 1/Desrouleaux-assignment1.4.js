4ae0cfb05b3dcd9fa3b253435cbe6f19e3b7dd96
Lance Desrouleaux <ldesrouleaux@my365.bellevue.edu>
2019-02-11 11:49:25 -0500

    New file added ( desrouleaux-assignment1.4)

---
+++ Week 1/Desrouleaux-assignment1.4.js
@@ -0,0 +1,14 @@
+var firstName = "Lance";
+var lastName = "Desrouleaux";
+var age = "32";
+var street = "123 Main Street";
+var city = " Wellington";
+var zipCode = "56433";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
+
