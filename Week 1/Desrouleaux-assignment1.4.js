74bf1f3f9ad8ce62fe9c851077058e5b7a57ca1d
Lance Desrouleaux <ldesrouleaux@my365.bellevue.edu>
2019-02-13 08:31:26 -0500

    Updated all js files with headers

--- Week 1/Desrouleaux-assignment1.4.js
+++ Week 1/Desrouleaux-assignment1.4.js
@@ -1,3 +1,26 @@
+/* 
+============================================
+; Title: Desrouleaux-assignment1.4.js
+; Author: Lance Desrouleaux
+; Date: 7 February 2019
+; Description: The purpose of this program is to establish and demonstrate the use of Javascript variables and value to display information about a user
+; 
+;===========================================
+*/ 
+
+
+/*
+    Expected Output:
+    First Name
+    Last Name
+    Age
+    Street Address
+    City
+    Zip Code
+*/
+
+
+//This is where I created the variables
 var firstName = "Lance";
 var lastName = "Desrouleaux";
 var age = "32";
@@ -5,10 +28,11 @@ var street = "123 Main Street";
 var city = " Wellington";
 var zipCode = "56433";
 
-console.log(firstName);
-console.log(lastName);
-console.log(age);
-console.log(street);
-console.log(city);
-console.log(zipCode);
+//This is where the variables are displayed to screen 
+console.log("First Name: " + firstName);
+console.log("Last Name: "+ lastName);
+console.log("Age: "+ age);
+console.log("Street: " + street);
+console.log("City: " + city);
+console.log("Zip Code: " + zipCode);
 
--- Week 1/Desrouleaux-assignment1.5.js
+++ Week 1/Desrouleaux-assignment1.5.js
@@ -1,56 +1,96 @@
+/* 
+============================================
+; Title: Desrouleaux-assignment-.5.js
+; Author: Lance Desruleaux
+; Date: 11 February 2019
+; Description: Using Javascript to store and display employee information
+;===========================================
+*/ 
 
 
-//first names
+
+/*
+    Expected Output:
+    FOR EACH EMPLOYEE:
+    Employee Number
+    First Name
+    Last Name
+    Address
+    Pay Rate
+    Hire Date
+*/
+
+
+
+//This is where the first names are stored 
 var firstNameOne= "Bob";
 var firstNameTwo = "John";
 var firstNameThree = "Teddy";
 
-//last names
+//This is where the last names are stored
 var lastNameOne = "Dole"
 var lastNameTwo = "Legend"
 var lastNameThree = "Roosevelt"
 
 
-//Addresses
+
+
+
+//Here is where Addresses are stored
 var addressOne = "123 beach Road";
 var addressTwo = "345 LongStride Circle";
 var addressThree = "6778 Bilmpount court";
 
-//Pay Rates
+//this is where the Pay Rates are stored 
 var payRateOne = 12.658
 var payRateTwo = 15.77
 var payRateThree = 78.44
 
-//hire date
+//This is where the hire dates are stored
 var hireDateOne = new Date(2010, 11, 2);
 var hireDateTwo = new Date(1999, 22, 3);
 var hireDateThree = new Date(1998, 8, 10);
 
 
 
-// Format Hire Dates to  string with slashes
+// This is where I Format Hire Dates to string with slashes
 hireDateOne = hireDateOne.toLocaleDateString();
 hireDateTwo = hireDateTwo.toLocaleDateString();
 hireDateThree = hireDateThree.toLocaleDateString();
 
 
-//Adjust pay rate to 2 decimal digits
+//Here I use a dunction to adjust pay rate to 2 decimal digits for each employee
 payRateOne = payRateOne.toFixed(2);
 payRateOne = payRateTwo.toFixed(2);
 payRateOne = payRateThree.toFixed(2);
 
 
-//Log all the Employees
-
-console.log("First Name:" + firstNameOne + " Last Name: " + lastNameOne + " Address: " + addressOne + " Hire Date: " + hireDateOne + " Pay Rate:$"+ payRateOne);
-
-console.log("First Name:" + firstNameTwo+ " Last Name: " + lastNameTwo + " Address: " + addressTwo + " Hire Date: " + hireDateTwo + " Pay Rate:$"+ payRateTwo);
+// Here all employees information is logged to screen
 
 
-console.log("First Name:" + firstNameThree + " Last Name: " + lastNameThree + " Address: " + addressThree + " Hire Date:" + hireDateThree +  " Pay Rate:$"+ payRateThree);
+console.log('Employee 1');
+console.log('First Name: ' + firstNameOne);
+console.log('Last Name: ' + lastNameOne);
+console.log('Address: ' + addressOne);
+console.log('Daily Compensation: $'+payRateOne);
+console.log ('Hire Date: ' + hireDateOne);
 
+console.log(""); // new line
 
+console.log('Employee 2');
+console.log('First Name: ' + firstNameTwo);
+console.log('Last Name: ' + lastNameTwo);
+console.log('Address: ' + addressTwo);
+console.log('Daily Compensation: $'+payRateTwo);
+console.log ('Hire Date: '+hireDateTwo);
 
+console.log(""); // new line
 
+console.log('Employee 3');
+console.log('First Name: '+firstNameThree);
+console.log('Last Name: '+lastNameThree);
+console.log('Address: '+addressThree);
+console.log('Daily Compensation: $'+payRateThree);
+console.log ('Hire Date: '+hireDateThree);
 
 
